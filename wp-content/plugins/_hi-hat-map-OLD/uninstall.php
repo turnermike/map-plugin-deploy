391a9a7b98ffdf647ec8ea195866f531fc44aa53
Mike Turner <mturner@realinteractive.ca>
2015-11-18 14:04:28 -0500

    added plugin bootstrap

---
+++ wp-content/plugins/_hi-hat-map-OLD/uninstall.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Hi-hat Map
+ *
+ * @package   Hi_Hat_Map
+ * @author    Mike Turner <turner.mike@gmail.com>
+ * @license   GPL-2.0+
+ * @link      http://hi-hatconsulting.com
+ * @copyright 2014 Hi-hat Consulting
+ */
+
+// If uninstall not called from WordPress, then exit
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit;
+}
+
+global $wpdb;
+
+if ( is_multisite() ) {
+
+	$blogs = $wpdb->get_results( "SELECT blog_id FROM {$wpdb->blogs}", ARRAY_A );
+	if ( $blogs ) {
+
+	 	foreach ( $blogs as $blog ) {
+			switch_to_blog( $blog['blog_id'] );
+			restore_current_blog();
+		}
+	}
+
+}
--- wp-content/plugins/hi-hat-map/uninstall.php
+++ wp-content/plugins/hi-hat-map/uninstall.php
@@ -1,30 +1,31 @@
 <?php
+
 /**
- * Hi-hat Map
+ * Fired when the plugin is uninstalled.
  *
- * @package   Hi_Hat_Map
- * @author    Mike Turner <turner.mike@gmail.com>
- * @license   GPL-2.0+
- * @link      http://hi-hatconsulting.com
- * @copyright 2014 Hi-hat Consulting
+ * When populating this file, consider the following flow
+ * of control:
+ *
+ * - This method should be static
+ * - Check if the $_REQUEST content actually is the plugin name
+ * - Run an admin referrer check to make sure it goes through authentication
+ * - Verify the output of $_GET makes sense
+ * - Repeat with other user roles. Best directly by using the links/query string parameters.
+ * - Repeat things for multisite. Once for a single site in the network, once sitewide.
+ *
+ * This file may be updated more in future version of the Boilerplate; however, this is the
+ * general skeleton and outline for how the file should work.
+ *
+ * For more information, see the following discussion:
+ * https://github.com/tommcfarlin/WordPress-Plugin-Boilerplate/pull/123#issuecomment-28541913
+ *
+ * @link       http://example.com
+ * @since      1.0.0
+ *
+ * @package    Plugin_Name
  */
 
-// If uninstall not called from WordPress, then exit
+// If uninstall not called from WordPress, then exit.
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
-}
-
-global $wpdb;
-
-if ( is_multisite() ) {
-
-	$blogs = $wpdb->get_results( "SELECT blog_id FROM {$wpdb->blogs}", ARRAY_A );
-	if ( $blogs ) {
-
-	 	foreach ( $blogs as $blog ) {
-			switch_to_blog( $blog['blog_id'] );
-			restore_current_blog();
-		}
-	}
-
 }
